1fb92de645ea12ccc7e1a191ebfa29f45df18cdc
jalalk1244 <jalaluddin.boss@gmail.com>
2023-01-12 10:49:40 +0000

    Create page header for the products page

--- products/views.py
+++ products/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from .models import Product, Category, Subcategory
+
 
 # Create your views here.
 def products(request):
     '''View for the products page'''
-    return render(request, 'products/products.html')
+    category = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            category = request.GET['category']
+            products = Product.objects.filter(category__slug=category)
+            print(products)
+    else:
+        products = Product.objects.all()
+
+    subcategories = Subcategory.objects.filter(parent_category__slug=category)
+    context = {
+        'products': products,
+        'category': category,
+        'subcategories': subcategories,
+    }
+    return render(request, 'products/products.html', context)
