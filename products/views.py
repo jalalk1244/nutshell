d0d31f9a37df316a7f1e21525664998d9a9caa3a
jalalk1244 <jalaluddin.boss@gmail.com>
2023-01-12 23:15:09 +0000

    Add sorting based on price functionality

--- products/views.py
+++ products/views.py
@@ -6,14 +6,22 @@ from .models import Product, Category, Subcategory
 def products(request):
     '''View for the products page'''
     category = None
+    sort = None
+    sort_by = 'sku'
 
     if request.GET:
+        if 'sort' in request.GET:
+            sort_key = request.GET['sort']
+            if sort_key == 'l2h':
+                sort_by = 'price'
+            else:
+                sort_by = '-price'
+
         if 'category' in request.GET:
             category = request.GET['category']
-            products = Product.objects.filter(category__slug=category)
-            print(products)
-    else:
-        products = Product.objects.all()
+            products = Product.objects.filter(category__slug=category).order_by(sort_by)
+        else:
+            products = Product.objects.all().order_by(sort_by)
 
     subcategories = Subcategory.objects.filter(parent_category__slug=category)
     context = {
