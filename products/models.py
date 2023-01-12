88ec072acc49fb6293c7bbb34a2364805afbc1ba
jalalk1244 <jalaluddin.boss@gmail.com>
2023-01-12 07:41:33 +0000

    Create category and subcategory models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(null=False, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(null=False, unique=True)
