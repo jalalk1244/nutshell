7c60305df2257b1c4eb48e0312abe33d6166e629
jalalk1244 <jalaluddin.boss@gmail.com>
2023-01-12 07:23:51 +0000

    Create a new products app and add it to settings.py

--- nutshell/settings.py
+++ nutshell/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -128,8 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)	
-MEDIA_URL = '/media/'	
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
