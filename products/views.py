d7819077430ff5ecfd99c4cfca67dfd6c3d1974f
jalalk1244 <jalaluddin.boss@gmail.com>
2023-01-13 06:23:05 +0000

    Add sorting based on subcategory functionality

--- products/views.py
+++ products/views.py
@@ -5,9 +5,11 @@ from .models import Product, Category, Subcategory
 # Create your views here.
 def products(request):
     '''View for the products page'''
+    products = Product.objects.all()
     category = None
     sort = None
     sort_by = 'sku'
+    subcategories = []
 
     if request.GET:
         if 'sort' in request.GET:
@@ -16,14 +18,19 @@ def products(request):
                 sort_by = 'price'
             else:
                 sort_by = '-price'
+        products = Product.objects.all().order_by(sort_by)
 
         if 'category' in request.GET:
             category = request.GET['category']
-            products = Product.objects.filter(category__slug=category).order_by(sort_by)
-        else:
-            products = Product.objects.all().order_by(sort_by)
+            products = Product.objects.filter(
+                category__slug=category).order_by(sort_by)
+            subcategories = Subcategory.objects.filter(
+                parent_category__slug=category)
+            if 'subcategory' in request.GET:
+                subcategory = request.GET['subcategory']
+                products = Product.objects.filter(
+                    sub_category__slug=subcategory).order_by(sort_by)
 
-    subcategories = Subcategory.objects.filter(parent_category__slug=category)
     context = {
         'products': products,
         'category': category,
