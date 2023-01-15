4ad1df216e14677f0cf74d50211edab6e43c6835
jalalk1244 <jalaluddin.boss@gmail.com>
2023-01-15 13:50:41 +0000

    Add view, template and url for editing products

--- products/views.py
+++ products/views.py
@@ -163,3 +163,29 @@ def add_new_product(request):
     }
 
     return render(request, 'products/add_product.html', context)
+
+
+@login_required
+def edit_product(request, product_id):
+    '''A view to add new products to the store'''
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'products/edit_product.html', context)
