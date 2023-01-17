fb2fd40cdcd2a3bc5d2a95f65afaa3a37421ec2c
jalalk1244 <jalaluddin.boss@gmail.com>
2023-01-17 23:16:10 +0000

    Add order history to the profile page and link to order_details

--- profiles/views.py
+++ profiles/views.py
@@ -1,19 +1,47 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
+
+from checkout.models import Order
 
 
 def profile(request):
     '''View for rendering the profile page'''
     profile = get_object_or_404(UserProfile, user=request.user)
 
-    form = UserProfileForm(instance=profile)
-    orders = profile.orders.all()
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
 
+    orders = profile.orders.all()
     context = {
         'form': form,
         'profile': profile,
         'orders': orders,
     }
     return render(request, 'profiles/profile.html', context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the oder date'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True
+    }
+
+    return render(request, template, context)
