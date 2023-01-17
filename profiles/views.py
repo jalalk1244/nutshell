da777f2eb5ecf7e812532ad8c5386700f2f382f2
jalalk1244 <jalaluddin.boss@gmail.com>
2023-01-17 21:44:02 +0000

    Create UserProfileForm and render it in the profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     '''View for rendering the profile page'''
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     context = {
+        'form': form,
         'profile': profile,
+        'orders': orders,
     }
     return render(request, 'profiles/profile.html', context)
