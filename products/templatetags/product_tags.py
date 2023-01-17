b12ce79b63a02a20fb4e83cb1ffd4225cc44df99
jalalk1244 <jalaluddin.boss@gmail.com>
2023-01-17 19:34:57 +0000

    Add number of products on the shopping cart icon

--- products/templatetags/product_tags.py
+++ products/templatetags/product_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from ..models import ProductRating
+from bag.views import get_cart_info
 
 
 register = template.Library()
@@ -8,3 +9,13 @@ register = template.Library()
 @register.filter(name='calc_stars')
 def calc_stars(rating):
     return range(rating)
+
+
+@register.simple_tag()
+def number_of_cart_items(request):
+    num_of_items = get_cart_info(request)['product_count']
+
+    if num_of_items == 0:
+        return ''
+    else:
+        return num_of_items
