21d2b02cc175b0019381b7dc288d4a5457f9c970
jalalk1244 <jalaluddin.boss@gmail.com>
2023-01-18 08:39:45 +0000

    Remove the product description from the products page

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import ProductRating, Product, Category, Subcategory
 
+
 class ReviewForm(forms.ModelForm):
 
     class Meta:
@@ -13,7 +14,7 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
